276b2a532a8c2a55d24bfd81e6a2c0d14c8ce796
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-04-19 22:44:35 -0300

    feat(*): formulario de auth com react hook form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Button, Flex, Stack } from '@chakra-ui/react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { Input } from '../components/Form/Input'
 
 type SignInFormData = {
@@ -7,8 +9,17 @@ type SignInFormData = {
   password: string;
 }
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required().email(),
+  password: yup.string().required(),
+})
+
 export default function Signin() {
-  const {register, handleSubmit, formState} = useForm();
+  const {register, handleSubmit, formState} = useForm({
+    resolver: yupResolver(signInFormSchema)
+  });
+
+  const {errors} = formState;
 
   const handleSignin: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000))
@@ -18,8 +29,19 @@ export default function Signin() {
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex as="form" w="100%" maxW={360} bg="gray.800" p="8" borderRadius={8} flexDir="column" onSubmit={handleSubmit(handleSignin)}>
         <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" {...register('email')} />
-          <Input name="password" label="Senha" type="password" {...register('password')} />
+          <Input 
+            name="email" 
+            label="E-mail"
+            error={errors.email} 
+            type="email" 
+            {...register('email')} 
+          />
+          <Input 
+            name="password" 
+            label="Senha" 
+            type="password" 
+            error={errors.password} 
+            {...register('password')} />
         </Stack>
         <Button type="submit" mt="6" colorScheme="pink" size="lg" isLoading={formState.isSubmitting} >Entrar</Button>
       </Flex>
