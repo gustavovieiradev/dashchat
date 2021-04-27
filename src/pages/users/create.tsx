ab5eda9c1556f09f5283c5ee95dd29ace1aec4a9
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-04-27 09:20:05 -0300

    ajustes

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,59 +1,113 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, useToast, VStack } from "@chakra-ui/react";
 import Link from "next/link";
-import { SubmitHandler, useForm } from 'react-hook-form'
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
+import { useRouter } from "next/router";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { v4 } from 'uuid';
+import { Select } from "../../components/Form/Select";
+import { GetServerSideProps } from "next";
+import { fauna } from "../../services/fauna";
+import { query as q } from 'faunadb';
 
-type CreateUserFormData = {
+type UserFormData = {
+  id: string;
   name: string;
   email: string;
-  password: string;
-  password_confirmation: string;
+  id_client: string;
+  id_project: string;
+  client?: Client;
 }
 
-const createUserFormSchema = yup.object().shape({
-  name: yup.string().required(),
-  email: yup.string().required().email(),
-  password: yup.string().required(),
-})
+interface Client {
+  id: string;
+  value: string;
+}
 
-export default function UserCreate() {
+interface UserCreateProps {
+  clients: Client[];
+}
+
+export default function UserCreate({clients}: UserCreateProps) {
+  const router = useRouter();
+  const toast = useToast()
+  const {register, handleSubmit, formState} = useForm();
+
+  const {errors} = formState;
+
+  const handleSave: SubmitHandler<UserFormData> = async (values) => {
+    values.id = v4();
+    const client = clients.find(c => values.id_client === c.id);
+    values.client = client;
+
+    await api.post('/project/create', values);
+    toast({
+      title: "Projeto salvo com sucesso",
+      status: "success",
+      duration: 9000,
+      isClosable: true,
+    })
+    router.push('/projects');
+  } 
+
   return (
     <Box>
       <Header />
-      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+      <Flex as="form" w="100%" my="6" maxW={1480} mx="auto" px="6" onSubmit={handleSubmit(handleSave)}>
         <Sidebar />
         <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
-          <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
-
+          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
           <Divider my="6" borderColor="gray.700" />
-
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="name" label="Nome completo" />
-              <Input name="email" type="email" label="E-mail" />
-            </SimpleGrid>
-            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmação de senha" />
-            </SimpleGrid>
-
+            <Box w="100%">
+              <Input name="name" label="Nome do usuário" {...register('name')} />
+            </Box>
+            <Box w="100%">
+              <Input name="name" label="E-mail" {...register('email')} />
+            </Box>
+            <Box w="100%">
+              <Select placeholder="Selecione" name="client" label="Cliente" {...register('id_client')} options={clients} />
+            </Box>
+            <Box w="100%">
+              <Select placeholder="Selecione" name="client" label="Projetos" {...register('id_project')} options={clients} />
+            </Box>
           </VStack>
-
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Link href="/users" passHref>
+              <Link href="/client" passHref>
                 <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button colorScheme="pink">Salvar</Button>
+              <Button colorScheme="pink" isLoading={formState.isSubmitting} type="submit">Salvar</Button>
             </HStack>
           </Flex>
-
         </Box>
       </Flex>
     </Box>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async() => {
+  const response: any = await fauna.query(
+    q.Map(
+      q.Paginate(
+        q.Match(q.Index('ix_client')),
+      ),
+      q.Lambda("X", q.Get(q.Var("X")))
+    )
+  )
+
+  const clients = response.data.map(res => {
+    return {
+      id: res.data.id,
+      value: res.data.name,
+    }
+  });
+  
+  return {
+    props: {
+      clients,
+    }
+  }
 }
---
+++ src/pages/users/create_old.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import Link from "next/link";
+import { SubmitHandler, useForm } from 'react-hook-form'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().required().email(),
+  password: yup.string().required(),
+})
+
+export default function UserCreate() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+          <Heading size="lg" fontWeight="normal">Criar Usuário</Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input name="name" label="Nome completo" />
+              <Input name="email" type="email" label="E-mail" />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
+              <Input name="password" type="password" label="Senha" />
+              <Input name="password_confirmation" type="password" label="Confirmação de senha" />
+            </SimpleGrid>
+
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
