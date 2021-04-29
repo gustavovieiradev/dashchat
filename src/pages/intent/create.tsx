6ccb387c4a235ad6b1b2b47e6faef24fcab78cc5
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-04-28 21:26:37 -0300

    ajustes

--- src/pages/intent/create.tsx
+++ src/pages/intent/create.tsx
@@ -11,6 +11,8 @@ import { v4 } from 'uuid';
 import { GetServerSideProps } from "next";
 import { fauna } from "../../services/fauna";
 import { query as q } from 'faunadb';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type IntentFormData = {
   name: string;
@@ -29,10 +31,19 @@ interface CreateIntentProps {
   projects: Project[];
 }
 
+const formSchema = yup.object().shape({
+  id_project: yup.string().required('Campo obrigatório'),
+  name: yup.string().required('Campo obrigatório'),
+  text_input: yup.string().required('Campo obrigatório'),
+  text_output: yup.string().required('Campo obrigatório'),
+})
+
 export default function CreateIntent({projects}: CreateIntentProps) {
   const router = useRouter();
   const toast = useToast()
-  const {register, handleSubmit, formState} = useForm();
+  const {register, handleSubmit, formState} = useForm({
+    resolver: yupResolver(formSchema)
+  });
 
   const {errors} = formState;
 
@@ -66,16 +77,16 @@ export default function CreateIntent({projects}: CreateIntentProps) {
           <Divider my="6" borderColor="gray.700" />
           <VStack spacing="8">
             <Box w="100%">
-              <Select name="next_intent" label="Projeto" options={projects} {...register('id_project')}/>
+              <Select name="next_intent" placeholder="Selecione" label="Projeto" options={projects} {...register('id_project')} error={errors.id_project} />
             </Box>
             <Box w="100%">
-              <Input name="name" label="Nome da intenção" {...register('name')} />
+              <Input name="name" label="Nome da intenção" {...register('name')} error={errors.name} />
             </Box>
             <Box w="100%">
-              <Input name="text_input" label="Texto de entrada" {...register('text_input')} />
+              <Input name="text_input" label="Texto de entrada" {...register('text_input')} error={errors.text_input} />
             </Box>
             <Box w="100%">
-              <Input name="text_output" label="Texto de saída" {...register('text_output')} />
+              <Input name="text_output" label="Texto de saída" {...register('text_output')} error={errors.text_output} />
             </Box>
           </VStack>
           <Flex mt="8" justify="flex-end">
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -12,6 +12,8 @@ import { GetServerSideProps } from "next";
 import { fauna } from "../../services/fauna";
 import { query as q } from 'faunadb';
 import { useState } from "react";
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type UserFormData = {
   id: string;
@@ -39,10 +41,19 @@ interface UserCreateProps {
   clients: Client[];
 }
 
+const formSchema = yup.object().shape({
+  name: yup.string().required('Campo obrigatório'),
+  email: yup.string().required('Campo obrigatório'),
+  id_client: yup.string().required('Campo obrigatório'),
+  id_project: yup.string().required('Campo obrigatório'),
+})
+
 export default function UserCreate({clients}: UserCreateProps) {
   const router = useRouter();
   const toast = useToast()
-  const {register, handleSubmit, formState} = useForm();
+  const {register, handleSubmit, formState} = useForm({
+    resolver: yupResolver(formSchema)
+  });
   const [projects, setProjects] = useState<Project[]>([]);
 
   const {errors} = formState;
@@ -100,16 +111,16 @@ export default function UserCreate({clients}: UserCreateProps) {
           <Divider my="6" borderColor="gray.700" />
           <VStack spacing="8">
             <Box w="100%">
-              <Input name="name" label="Nome do usuário" {...register('name')} />
+              <Input name="name" label="Nome do usuário" {...register('name')} error={errors.name} />
             </Box>
             <Box w="100%">
-              <Input name="name" label="E-mail" {...register('email')} />
+              <Input name="name" label="E-mail" {...register('email')} error={errors.email} />
             </Box>
             <Box w="100%">
-              <Select placeholder="Selecione" name="client" label="Cliente" {...register('id_client')} options={clients} onChange={(ev) => handleChangeClient(ev.target.value)} />
+              <Select placeholder="Selecione" name="client" label="Cliente" {...register('id_client')} options={clients} onChange={(ev) => handleChangeClient(ev.target.value)} error={errors.id_client}/>
             </Box>
             <Box w="100%">
-              <Select placeholder="Selecione" name="client" label="Projetos" {...register('id_project')} options={projects} />
+              <Select placeholder="Selecione" name="project" label="Projetos" {...register('id_project')} options={projects} error={errors.id_project}/>
             </Box>
           </VStack>
           <Flex mt="8" justify="flex-end">
