173e67d6b69f81e0b9607d850b18ee9cb7711ef6
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-04-30 10:16:35 -0300

    implementacao de logout

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -2,14 +2,26 @@ import { Stack } from "@chakra-ui/react";
 import { RiContactsLine, RiDashboardLine, RiGitMergeLine, RiInputMethodLine } from "react-icons/ri";
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
+import { Icon, Link, Text, LinkProps } from "@chakra-ui/react";
+import { useCookies } from "react-cookie";
+import { useRouter } from "next/router";
 
 export function SidebarNav() {
+  const [cookies, setCookie, removeCookie] = useCookies(['user']);
+  const router = useRouter();
+
+  function logout() {
+    removeCookie('user');
+    removeCookie('token');
+    router.push('/');
+  }
+
   return (
     <Stack spacing="12" align="flex-start">
       <NavSection title="CHAT">
         <NavLink href="/chat" icon={RiDashboardLine} children="Chat" />
         <NavLink href="/intent" icon={RiDashboardLine} children="Intenções" />
-        <NavLink href="/conversations" icon={RiDashboardLine} children="Conversas" />
+        <NavLink href="/conversas" icon={RiDashboardLine} children="Conversas" />
         <NavLink href="/preview" icon={RiDashboardLine} children="Preview" />
       </NavSection>
 
@@ -19,7 +31,10 @@ export function SidebarNav() {
         <NavLink href="/projects" icon={RiContactsLine} children="Projetos" />
       </NavSection>
 
-      <NavLink href="/" icon={RiContactsLine} children="Sair" exact={true} />
+      <Link as="button" display="flex" alignItems="center" onClick={logout}>
+        <Icon as={RiContactsLine} fontSize="20" />
+        <Text ml="4" fontWeight="medium">Sair</Text>
+      </Link>
     </Stack>
   )
 }
