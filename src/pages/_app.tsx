5a223dc93f359efb7e5c20910e6c68b8d51de7aa
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-05-02 18:36:27 -0300

    adicionando progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,21 @@ import { AppProps } from "next/app";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 import { UserProvider } from "../contexts/UserContext";
 import { theme } from "../styles/theme";
+import Router from "next/router";
+import ProgressBar from "@badrap/bar-of-progress";
+
+import '../styles/global.scss';
+
+const progress = new ProgressBar({
+  size: 3,
+  color: "#D53F8C",
+  delay: 100,
+  className: 'bar-of-progress'
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
